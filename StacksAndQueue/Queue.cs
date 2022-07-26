b4bc1ac5fbc78ecc003cb1728a178dc67959f817
Megha8150 <meghananayak436@gmail.com>
2022-07-27 01:26:58 +0530

    Created queue

---
+++ StacksAndQueue/Queue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueue
+{
+    public class Queue
+    {
+        public LinkedList linkedlist;
+
+        public Queue()
+        {
+            this.linkedlist = new LinkedList();
+        }
+        public void Enqueue(int data)
+        {
+          linkedlist.Append(data);
+        }
+
+        public void print()
+        {
+            linkedlist.display();
+        }
+
+    }
+}
--- StacksAndQueue/Stack.cs
+++ StacksAndQueue/Stack.cs
@@ -43,5 +43,8 @@ namespace StacksAndQueue
         {
             return linkedlist.pop();
         }
+
+      
+       
     }
 }
