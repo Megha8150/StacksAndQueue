d94fe3ca02e7bbde240fa15a15aa8fde8325c68f
Megha8150 <meghananayak436@gmail.com>
2022-07-27 00:53:35 +0530

    Created Stack

---
+++ StacksAndQueue/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StacksAndQueue;
+
+Stack stack = new Stack();
+stack.push(70);
+stack.push(30);
+stack.push(56);
+stack.print();
---
+++ StacksAndQueue/Stack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace StacksAndQueue
+{
+    public class Stack
+    {
+       public LinkedList linkedlist;
+
+        public Stack()
+        {
+            this.linkedlist = new LinkedList();
+        }
+
+        public void push(int data)
+        {
+            linkedlist.add(data);
+        }
+
+        public void print()
+        {
+            linkedlist.display();
+        }
+    }
+}
